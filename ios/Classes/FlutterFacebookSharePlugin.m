ebc95da28953d137934d6fcbdc0466fb766122f4
james <james@yodelit.com.au>
2020-11-20 10:54:15 +1000

    cc

--- ios/Classes/FlutterFacebookSharePlugin.m
+++ ios/Classes/FlutterFacebookSharePlugin.m
@@ -1,14 +1,14 @@
-#import "FlutterFacebookAuthPlugin.h"
-// #if __has_include(<flutter_facebook_auth/flutter_facebook_auth-Swift.h>)
-#import <flutter_facebook_auth/flutter_facebook_auth-Swift.h>
-// #else
+#import "FlutterFacebookSharePlugin.h"
+#if __has_include(<flutter_facebook_share/flutter_facebook_share-Swift.h>)
+#import <flutter_facebook_share/flutter_facebook_share-Swift.h>
+#else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-// #import "flutter_facebook_auth-Swift.h"
-// #endif
+#import "flutter_facebook_share-Swift.h"
+#endif
 
-@implementation FlutterFacebookAuthPlugin
+@implementation FlutterFacebookSharePlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   [SwiftFlutterFacebookSharePlugin registerWithRegistrar:registrar];
 }
