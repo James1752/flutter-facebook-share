c743b17923535b9ac05030c7fa938ab1f0281a20
james <james@yodelit.com.au>
2020-11-20 11:11:50 +1000

    c

--- ios/Classes/FlutterFacebookSharePlugin.m
+++ ios/Classes/FlutterFacebookSharePlugin.m
@@ -1,11 +1,11 @@
 #import "FlutterFacebookSharePlugin.h"
-#if __has_include(<flutter_facebook_share/flutter_facebook_share-Swift.h>)
-#import <flutter_facebook_share/flutter_facebook_share-Swift.h>
+#if __has_include(<social_share/social_share-Swift.h>)
+#import <social_share/social_share-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "flutter_facebook_share-Swift.h"
+#import "social_share-Swift.h"
 #endif
 
 @implementation FlutterFacebookSharePlugin
