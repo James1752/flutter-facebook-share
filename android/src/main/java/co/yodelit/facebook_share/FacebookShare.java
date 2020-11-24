cd03f2f22926209038573006be5d72c097b5ec56
james <james@yodelit.com.au>
2020-11-24 11:28:01 +1000

    changes

--- android/src/main/java/co/yodelit/facebook_share/FacebookShare.java
+++ android/src/main/java/co/yodelit/facebook_share/FacebookShare.java
@@ -23,8 +23,8 @@ public class FacebookShare {
 
     void isFacebookInstalled(Activity activity, MethodChannel.Result result) {
         final boolean isFbInstalled = isPackageInstalled(activity, "com.facebook.katana");
-        Log.d("myTag", "This is my message");
-        Log.d("myTag", isFbInstalled);
+        print("This is my message");
+        print(isFbInstalled);
            result.success(isFbInstalled);
     }
 
